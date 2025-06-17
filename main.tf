9bbb59b5400e138eb9d218b5edbc1b5e30d7e4d8
Ankit <ankit231992@gmail.com>
2025-06-17 19:44:17 +0530

    Removed 2 instances

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0f918f7e67a3323f0"
-  instance_type = "t2.micro"
+# resource "aws_instance" "web" {
+#   ami           = "ami-0f918f7e67a3323f0"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "ankit_tf_first"
-  }
-}
+#   tags = {
+#     Name = "ankit_tf_first"
+#   }
+# }
