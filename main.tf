502279c37b81cc081782fb948c4dd6e76468e51f
Ankit <ankit231992@gmail.com>
2025-06-17 23:23:48 +0530

    added provider.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.0.0-beta3"
-    }
+resource "aws_instance" "web" {
+  ami           = "ami-0f918f7e67a3323f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ankit_tf_first"
   }
 }
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
-# resource "aws_instance" "web" {
-#   ami           = "ami-0f918f7e67a3323f0"
-#   instance_type = "t2.micro"
-
-#   tags = {
-#     Name = "ankit_tf_first"
-#   }
-# }
