de8b1887c71a57753f047a00ffdd82a6b23e79e6
Ankit <ankit231992@gmail.com>
2025-06-18 00:34:57 +0530

    Created new s3 bucket

--- s3.tf
+++ s3.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "ankit_bucket" {
-  bucket = "ankit-bucket-nrzb1234"
-}
+# resource "aws_s3_bucket" "ankit_bucket" {
+#   bucket = "ankit-bucket-nrzb1234"
+# }
 
-resource "aws_s3_object" "Upload_my_file" {
-  bucket = aws_s3_bucket.ankit_bucket.bucket
-  source = "./S3_data/ankit.txt"
-  key    = "myfile.txt"
-}
+# resource "aws_s3_object" "Upload_my_file" {
+#   bucket = aws_s3_bucket.ankit_bucket.bucket
+#   source = "./S3_data/ankit.txt"
+#   key    = "myfile.txt"
+# }
 
