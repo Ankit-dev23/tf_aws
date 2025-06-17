760e7b734962011859e5f319d114c7d250878aae
Ankit <ankit231992@gmail.com>
2025-06-18 01:07:52 +0530

    Created new s3 bucket

--- providers.tf
+++ providers.tf
@@ -5,11 +5,11 @@ terraform {
       version = "6.0.0-beta3"
     }
   }
-    backend "s3" {
-      bucket = aws_s3_bucket.ankit_bucket.bucket
-      key    = "backend.tfstate"
-      region = var.region
-    }
+  backend "s3" {
+    bucket = aws_s3_bucket.ankit_bucket.bucket
+    key    = "backend.tfstate"
+    region = var.region
+  }
 }
 
 provider "aws" {
