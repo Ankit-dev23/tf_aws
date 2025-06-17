874bd44e171f7d76e2b71c97def9f175a3cb6a62
Ankit <ankit231992@gmail.com>
2025-06-17 19:41:11 +0530

    Removed 2 instances

--- main.tf
+++ main.tf
@@ -19,21 +19,3 @@ resource "aws_instance" "web" {
     Name = "ankit_tf_first"
   }
 }
-
-resource "aws_instance" "web2" {
-  ami           = "ami-0f918f7e67a3323f0"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ankit_tf_first2"
-  }
-}
-
-resource "aws_instance" "web3" {
-  ami           = "ami-0f918f7e67a3323f0"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ankit_tf_first3"
-  }
-}
