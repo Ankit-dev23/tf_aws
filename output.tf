459b7776e724f56dc46a9ca3e8340292d056af78
Ankit <ankit231992@gmail.com>
2025-06-17 23:42:52 +0530

    Fixed lint

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "ec2_instance_public_ip" {
   value       = aws_instance.web.public_ip
-  sensitive   = true
+  sensitive   = false
   description = "This is instance public IP"
   depends_on  = [aws_instance.web]
 }
