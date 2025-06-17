aa6f4e9b327d88e82d5c2c145e862f7dfb7a5527
Ankit <ankit231992@gmail.com>
2025-06-17 23:44:44 +0530

    Fixed lint

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0f918f7e67a3323f0"
-  instance_type = "t2.micro"
+# resource "aws_instance" "web" {
+#   ami           = "ami-0f918f7e67a3323f0"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "ankit_tf_first"
-  }
-}
+#   tags = {
+#     Name = "ankit_tf_first"
+#   }
+# }
